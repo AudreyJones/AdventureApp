0c069c04995862134e75994b37b19a183167dcaa
AudreyJones <audrey.eliz.jones@gmail.com>
2019-05-15 12:42:18 -0400

    Seeded Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Hero.destroy_all
+Villain.destroy_all
+Franchise.destroy_all
+Adventure.destroy_all
+
+
+
+batman = Hero.create(name: "Batman", abilities: "cool gadgets, money", catchphrase: "I am the hero Gotham needs")
+superman = Hero.create(name: "Superman", abilities: "strength, flight, charming", catchphrase: "I love Lois Lane")
+hanna = Hero.create(name: "Hannah", abilities: "arcane magic", catchphrase: "I was just tired, what can I say?")
+thor = Hero.create(name: "Thor", abilities: "strength, flight, hammer, thunder, lightening", catchphrase: "Do we have more ale?")
+
+loki = Villain.create(name: "Loki", abilities: "trickery, magic", catchphrase: "I should be king of Asgaard")
+joker = Villain.create(name: "Joker", abilities: "insane, bombs", catchphrase: "Hello Bats")
+nrygoth = Villain.create(name: "N’Rygoth", abilities: "interdimensional lovecraftian god", catchphrase: "I have many followers")
+thanos = Villain.create(name: "Thanos", abilities: "strength, army, infinity stones", catchphrase: "*snaps*")
+
+Franchise.create(name: "Marvel Cinematic Universe", company: "Marvel Studios")
+Franchise.create(name: "DC Extended Universe", company: "Warner Bros. Pictures")
+Franchise.create(name: "Rat Queens", company: "Darkhorse Comics")
+
+Adventure.create(name: "Endgame", location: "the Universe", franchise: Franchise.first, villain: Villain.fourth, hero: Hero.fourth)
+Adventure.create(name: "The Killing Joke", location: "Gotham", franchise: Franchise.second, villain: Villain.second, hero: Hero.first)
+Adventure.create(name: "Smallville", location: "Metropolis", franchise: Franchise.second, villain: Villain.first, hero: Hero.second)
+Adventure.create(name: "Sass & Sorcery", location:"medieval realm", franchise: Franchise.third, villain: Villain.third, hero: Hero.third)
+
+
+
