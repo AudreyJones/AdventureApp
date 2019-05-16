ff4484929a4b857d99d6fce4ebcaadd6aec4175a
AudreyJones <audrey.eliz.jones@gmail.com>
2019-05-16 14:11:51 -0400

    Removed trackable devise module from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable, :omniauthable
   has_many :adventures
   has_many :villains, through: :adventures
   has_many :franchises, through: :adventures
