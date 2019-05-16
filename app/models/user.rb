4e4158e7d4f9297c770df2464bdbb1578ab158f0
AudreyJones <audrey.eliz.jones@gmail.com>
2019-05-16 10:25:18 -0400

    Second half of OmniAuth-Github setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
+require 'pry'
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   has_many :adventures
   has_many :villains, through: :adventures
   has_many :franchises, through: :adventures
+
+  def self.from_omniauth(auth)
+    # binding.pry
+    where(provider: auth.provider, uid: auth.uid) first_or_create do |user|
+    user.provider = auth.provider
+    user.name = auth.info.name
+    user.uid = auth.uid
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
 end
