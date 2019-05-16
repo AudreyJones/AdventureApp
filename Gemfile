514610bd925264f25b7a547dad287ba17f0d141c
AudreyJones <audrey.eliz.jones@gmail.com>
2019-05-16 10:18:06 -0400

    Beginning the OmniAuth-Github process

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'devise'
 
+gem 'omniauth-github'
+
 gem 'pry'
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_165815) do
+ActiveRecord::Schema.define(version: 2019_05_16_141731) do
 
   create_table "adventures", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_05_15_165815) do
     t.string "last_name"
     t.text "abilities"
     t.text "catchphrase"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
