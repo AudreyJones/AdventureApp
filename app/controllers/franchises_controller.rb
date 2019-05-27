c48daf60115e2570d54885e99fdd6c59136770e8
AudreyJones <audrey.eliz.jones@gmail.com>
2019-05-27 12:12:42 -0400

    Took out unnecessary franchise_controller methods

--- app/controllers/franchises_controller.rb
+++ app/controllers/franchises_controller.rb
@@ -1,38 +1,38 @@
 class FranchisesController < ApplicationController
+    before_action :set_franchise, only: [:show]
     def index
 
     end
 
     def show
-        @franchise = Franchise.find_by_id(params[:id])
+    
     end
     
-    def new
-        @franchise = Franchise.new
-    end
+# Franchises are permanent and unable to be edited by the user
+    # def new
+    #     @franchise = Franchise.new
+    # end
 
-    def create
-        @franchise = Franchise.new(franchise_params)
-        # binding.pry
-        if !@franchise.valid?
-            flash[:alert] = "Franchise info invalid"
-            render :new
-        else
-            @franchise.save
-            redirect_to franchise_path(@franchise)
-        end
-    end
+    # def create
+    #     @franchise = Franchise.new(franchise_params)
+    #     # binding.pry
+    #     if !@franchise.valid?
+    #         flash[:alert] = "Franchise info invalid"
+    #         render :new
+    #     else
+    #         @franchise.save
+    #         redirect_to franchise_path(@franchise)
+    #     end
+    # end
 
-    def edit
-        @franchise = Franchise.find_by_id(params[:id])
-    end
-
-    def destroy
-
-    end
+    
     private
 
-    def franchise_params
-        params.require(:adventure).permit(:name, :company)
+    # def franchise_params
+    #     params.require(:adventure).permit(:name, :company)
+    # end
+
+    def set_franchise
+        @franchise = Franchise.find_by_id(params[:id])
     end
 end
