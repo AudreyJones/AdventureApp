6cc345adbd7a30accfbc22eb44dfa03e415d5ac7
AudreyJones <audrey.eliz.jones@gmail.com>
2019-06-12 14:08:21 -0400

    Beginning to flesh out the franchise.js file

--- app/assets/javascripts/franchises.js
+++ app/assets/javascripts/franchises.js
@@ -1,5 +1,31 @@
 $(function () {
-    console.log('franchises.js is loaded')
+    console.log('franchises.js is loaded!')
 });
 
-function listenForClick
+function listenForClick() {
+    $().on('click', function (event) {
+        event.preventDefault()
+        getFranchises()
+    })
+}
+
+function getFranchises() {
+    $.ajax({
+        url:'http://localhost:3000/franchises',
+        method: 'get',
+        dataType: 'json'
+    }).done(function (data)) {
+        console.log("The data is: ", data)
+        let myFranchise = new Franchise(data[0])
+        let myFranchiseHTML = myFranchise.postHTML()
+        document.getElementById()
+    }
+}
+
+class Franchise {
+    constructor(obj) {
+        this.id = obj.id
+        this.name = obj.name
+        
+    }
+}
