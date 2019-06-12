ade45a55b1a8df2593bdb49a3d039e893c228685
AudreyJones <audrey.eliz.jones@gmail.com>
2019-06-12 13:54:55 -0400

    Added .js files for all models

---
+++ app/assets/javascripts/franchises.js
@@ -0,0 +1,5 @@
+$(function () {
+    console.log('franchises.js is loaded')
+});
+
+function listenForClick
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :first_name, :last_name, :nickname, :abilities, :catchphrase
+# Associations
+  has_many :adventures
+  has_many :villains, through: :adventures
+  has_many :franchises, through: :adventures
 end
